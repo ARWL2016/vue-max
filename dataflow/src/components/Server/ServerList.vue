af1c2650941f92c40af95ea204ab561d57b0faf9
Alistair Willis <alistairrwillis@gmail.com>
2017-12-01 16:10:50 +0900

    add dataflow

---
+++ dataflow/src/components/Server/ServerList.vue
@@ -0,0 +1,16 @@
+<template>
+	<li class="list-group-item" v-on:click="resetStatus">
+		Server #{{ id }} Status: {{ status}} 
+	</li>
+</template>
+
+<script>
+	export default {
+		props: ['id', 'status'], 
+		methods: {
+			resetStatus() {
+				this.$emit('resetStatus', this.id);
+			}
+		}
+	}
+</script>
---
+++ dataflow/src/components/Server/Servers.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <server-list v-for="server in servers" :id="server.id" :status="server.status" @resetStatus="resetStatus($event)"></server-list>
+        </ul>
+    </div>
+</template>
+
+<script>
+	import ServerList from './ServerList.vue';
+	export default {
+		components: {
+			'server-list': ServerList
+		},
+		data: function() {
+			return {
+				servers: [
+					{id: 1, status: 'Normal'},
+					{id: 2, status: 'Critical'}, 
+					{id: 3, status: 'Unknown'}, 
+					{id: 4, status: 'Normal'} 
+				]
+			}
+		}, 
+		methods: {
+			resetStatus(id) {
+				console.log({id});
+				this.servers.forEach(server => {
+					if (server.id === id) {
+						server.status = 'Normal';
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
+
+<!--
+- loop through servers
+- create comp for the list item 
+- pass down the id and status 
+be able to click the component and change the status back to normal
+-->
